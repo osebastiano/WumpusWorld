d267d858be92fe74eac5e6762e0f70ce0ff4658d
sebastian <32945736+osebastiano@users.noreply.github.com>
2017-12-30 17:16:07 -0500

    Update EEPROM.h

--- EEPROM.h
+++ EEPROM.h
@@ -1,44 +1,47 @@
 /**************************************************************************/
 /**************************************************************************/
-/***** 			 PONTIFICIA UNIVERSIDAD CAT”LICA DEL PER⁄ 			  *****/
-/***** 				 FACULTAD DE CIENCIAS E INGENIERÕA 				  *****/
-/***** 						SISTEMAS DIGITALES 						  *****/
+/***** 			     PONTIFICIA UNIVERSIDAD CAT√ìLICA DEL PER√ö 		  	    *****/
+/***** 				        FACULTAD DE CIENCIAS E INGENIER√çA 				      *****/
+/***** 						            SISTEMAS DIGITALES 						          *****/
 /**************************************************************************/
-/***** Archivo: EEPROM.h 											  *****/
-/***** Microcontrolador: TM4C123GH6PM 								  *****/
-/***** MÛdulo(s) uC: EEPROM 										  *****/
-/***** Autor: Sebastian Caballa Barrientos 							  *****/
-/***** Fecha: Noviembre 2017 										  *****/
+/***** Archivo: EEPROM.h 											                        *****/
+/***** Microcontrolador: TM4C123GH6PM 								                *****/
+/***** M√≥dulo(s) uC: EEPROM 										                      *****/
+/***** Autor: Sebastian Caballa Barrientos 							              *****/
+/***** Fecha: Noviembre 2017 										                      *****/
 /**************************************************************************/
-/***** Funciones para configuracion, grabado y lectura en EEPROM.	  *****/
+/***** Funciones para configuracion, grabado y lectura en EEPROM.	    *****/
 /**************************************************************************/
 /**************************************************************************/
 #ifndef EEPROM_H_
 #define EEPROM_H_
 
-/***** 				 	EEPROM_inicializacion() 					  *****/
+/**************************************************************************/
+/***** 				 	          EEPROM_inicializacion() 					          *****/
 /**************************************************************************/
 /***** Configura el EEPROM indicando que el ancho de palabra es de 32 *****/
-/***** bits. 														  *****/
+/***** bits. 														                              *****/
 /**************************************************************************/
-/***** ENTRADAS: Ninguna 											  *****/
-/***** SALIDA: Ninguna 												  *****/
+/***** ENTRADAS: Ninguna 											                        *****/
+/***** SALIDA: Ninguna 												                        *****/
 /**************************************************************************/
 void EEPROM_inicializacion (void);
-/***** 				 		EEPROM_escribe() 					  	  *****/
 /**************************************************************************/
-/***** Guarda el Dato de 32 bits en la EEPROM en la ubicaciÛn deseada *****/
+/***** 				 		            EEPROM_escribe() 					  	          *****/
 /**************************************************************************/
-/***** ENTRADAS: Dato, N˙mero de Block, N˙mero de Offset			  *****/
-/***** SALIDA: Ninguna 												  *****/
+/***** Guarda el Dato de 32 bits en la EEPROM en la ubicaci√≥n deseada *****/
+/**************************************************************************/
+/***** ENTRADAS: Dato, N√∫mero de Block, N√∫mero de Offset			        *****/
+/***** SALIDA: Ninguna 												                        *****/
 /**************************************************************************/
 void EEPROM_escribe(uint32_t, uint16_t , uint8_t);
-/***** 				 			EEPROM_lee() 					  	  *****/
 /**************************************************************************/
-/***** Lee el Dato de 32 bits en la EEPROM en la ubicaciÛn deseada 	  *****/
+/***** 				 			            EEPROM_lee() 					  	            *****/
 /**************************************************************************/
-/***** ENTRADAS: N˙mero de Block, N˙mero de Offset			 		  *****/
-/***** SALIDA: Dato almacenado en EEPROM							  *****/
+/***** Lee el Dato de 32 bits en la EEPROM en la ubicaci√≥n deseada 	  *****/
+/**************************************************************************/
+/***** ENTRADAS: N√∫mero de Block, N√∫mero de Offset			 		          *****/
+/***** SALIDA: Dato almacenado en EEPROM							                *****/
 /**************************************************************************/
 uint32_t EEPROM_lee(uint16_t, uint8_t);
 
